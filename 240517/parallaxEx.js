986ca042e01315e9b21bf41e3b47cba00804bec8
danchu0214 <naeun5794@gmail.com>
2024-05-20 14:14:05 +0900

    240520 학습내용

--- 240517/parallaxEx.js
+++ 240517/parallaxEx.js
@@ -38,16 +38,74 @@ $('a[href="#"]').on('click', (e) => {
   
   
     // 해당 박스로 이동하면 내비에 .active 붙음
+    const idx = Math.floor(scrollY / $(window).height());
+    $('.side-menu li').removeClass('active').eq(idx).addClass('active');
+
+    // active가 있으면 li의 data-bg 적용
+    $('.side-menu li').each(function() {
+      const bgColor = $(this).data('bg');
+      // console.log(isBg)
+      /*
+      if($(this).hasClass('active')) {
+        $(this).find('.dot').css({backgroundColor : bgColor })
+      } else {
+        $(this).find('.dot').css({ backgroundColor : 'transparent' })
+      }
+      */
+     const isBg = $(this).hasClass('active');
+     $(this).find('.dot').css({ backgroundColor : isBg ? bgColor : 'transparent' });
+    });
   });
   
   // 내비 메뉴 클릭시 해당 박스로 부드럽게 이동
   $('.side-menu li').on('click', (e) => {
     const idx = $(e.currentTarget).index();
-    const currentBox = 100 * idx;
+    // const currentBox = 100 * idx; scrollTop, scrollTo 는 ex 반환 받음
+    const currentBox = $(window).height() * idx;
     console.log(currentBox);
     
+  //   scrollTo({
+  //     top: `${currentBox}vh`,
+  //     behavior: 'smooth'
+  //   })
+  
+  $('html, body').stop().animate({ scrollTop: currentBox }, 800, 'easeInOutBack');
+});
+
+//마우스 휠 이벤트
+$('.box').each(function() {
+  $(this).on('wheel',(e) => {
+    e.preventDefault();// 여기에서는 스크롤 기본 성격 제거
+
+    let delta = e.originalEvent.deltaY;// 스크롤 이동 거리
+    // const nextBox = this.nextElementSibling;
+    const nextBox = $(this).next()[0];
+    const prevBox = $(this).prev()[0];
+    let currentTop = null;
+
+    if(delta > 0) {//휠을 내렸을 때
+      // console.log(delta)
+      /*
+      if(nextBox) {
+        currentTop = nextBox.offsetTop;
+      } else {
+        currentTop = this.offsetTop;// 다음 박스의 값이 없는 경우 현재 박스의 높이를 반환
+        }
+       */
+
+        currentTop = nextBos ? nextBox.offsetTop : this.offsetTop;
+
+    } else {// 휠을 올렸을 때
+      if(prevBox) {
+        currentTop = prevBox.offsetTop;
+      } else {
+        // currentTop = 0; 첫번째 박스에 도달하면 높이값을 0으로 반환
+        return;// 이전 박스의 값이 없는 경우 벗어남
+      }
+    }
     scrollTo({
-      top: `${currentBox}vh`,
+      top: currentTop,
       behavior: 'smooth'
     })
-  });
+  })
+});
